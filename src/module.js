c1a4b19464bc5f6c02ff4804cbffee9b9f2c7a83
limin <limin@lianjia.com>
2017-07-18 13:53:57 +0800

    搭建webpack

---
+++ src/module.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class ModuleA extends Component{
+  render() {
+    return (
+      <div>
+        测试的
+      </div>
+    );
+  }
+}
+
+export default ModuleA
---
+++ webpack.prod.config.js
@@ -0,0 +1,59 @@
+var path = require('path');
+var webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: {
+    app:'./src/index.js',
+    module:'./src/module.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new webpack.optimize.CommonsChunkPlugin('common.js'),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    })
+  ],
+  devServer: {
+    hot: true, // 告诉 dev-server 我们在使用 HMR
+    contentBase: path.resolve(__dirname, 'dist'),
+    historyApiFallback: true,
+    publicPath: '/',
+    port:9100
+  },
+  module:{
+  	rules:[
+  		{
+  			test:/\.css$/,
+  			use:[
+  				'style-loader',
+  				'css-loader'
+  			]
+  		},
+  		{
+  			test:/\.(png|svg|jpg|gif|jpeg)$/,
+  			use:[
+  				'file-loader'
+  			]
+  		},
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+  	]
+  }
+};
